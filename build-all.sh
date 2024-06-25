1248b8b8570e696c76600c1590c6b35eac1ccbdb
Marcelo Varanda <m@varanda.ca>
2024-06-25 19:58:50 -0400

    update info

--- build-all.sh
+++ build-all.sh
@@ -8,8 +8,12 @@ echo "*********************************************"
 
 pushd ./armbian-build
 #./compile.sh BOARD=orangepizero2 BRANCH=current RELEASE=jammy
+# Ubuntu is offline as it fails to fetch: https://packages.ubuntu.com/jammy-updates/arm64/base-files/download
+
+#./compile.sh BOARD=orangepizero2 BRANCH=current RELEASE=focal EXPERT=yes
 
 # Note: jammy does not work as ubuntu repo is not always online... using bullseye
 #       However, fails if I try to have desktop manager... need to select console
 ./compile.sh BOARD=orangepizero2 BRANCH=current RELEASE=bullseye EXPERT=yes
 
+popd
